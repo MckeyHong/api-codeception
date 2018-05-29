ae6f10aca44c3fe9e3e39efd83becd7d6eb010b0
Mckey <pnv200414@hotmail.com>
2018-05-29 15:26:30 +0800

    增加取得資料失敗的測試

--- tests/api/CheckHiskioCoursesCest.php
+++ tests/api/CheckHiskioCoursesCest.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Codeception\Util\HttpCode;
 
 class CheckHiskioCoursesCest
 {
@@ -11,17 +12,34 @@ class CheckHiskioCoursesCest
     {
     }
 
-    // tests
-    public function tryToTest(ApiTester $I)
+    /**
+     * [Success] 檢查取得課程資料
+     *
+     * @param  ApiTester $I
+     */
+    public function testGetDataIsSuccess(ApiTester $I)
     {
         // 測試是否能成功取得 hiskio 的課程資料
         $I->sendGET('courses/professions?type=all&level=all&sort=latest&profession_id=1');
         // 檢查 HTTP status codes
-        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        $I->seeResponseCodeIs(HttpCode::OK);
         // 檢查回傳的Json 內容是否有符合指定的參數及數值
         $I->seeResponseContainsJson([
             'status'  => true,
             'message' => 'success'
         ]);
+    }
+
+    /**
+     * [Failed] 檢查取得課程資料
+     *
+     * @param  ApiTester $I
+     */
+    public function testGetDataIsFailed(ApiTester $I)
+    {
+        // 測試是否能成功取得 hiskio 的課程資料
+        $I->sendGET('courses/professions');
+        // 檢查 HTTP status codes
+        $I->seeResponseCodeIs(HttpCode::INTERNAL_SERVER_ERROR);
     }
 }
